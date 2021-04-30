ce05aca795a41abaedd8b585d1611ace81210465
Vijendra-Achar <vijendra.1995@gmail.com>
2021-04-30 23:55:07 +0530

    Created product details page and Product card component�

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ProductCard.scss';
+
+function ProductCard() {
+  return <div></div>;
+}
+
+export default ProductCard;
---
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './ProductDetails.scss';
+
+export default function ProductDetails() {
+  return <div></div>;
+}
