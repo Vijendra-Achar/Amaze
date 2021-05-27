e2469e43cc439892c6ee581fee87e720e082c44c
Vijendra-Achar <vijendra.1995@gmail.com>
2021-05-27 21:58:24 +0530

    Input field implemented ✔

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -9,14 +9,14 @@ export default class LoginPage extends Component {
         <div className="login__title"> Please sign in.</div>
         <br />
         <label className="material-input-outline" htmlFor="email_id">
-          <input id="email_id" className="login__email" type="text" />
+          <input id="email_id" className="login__email" type="text" required />
           <span className="placeholder">Email</span>
         </label>
 
         <br />
         <br />
         <label className="material-input-outline" htmlFor="password">
-          <input id="password" className="login__password" type="password" />
+          <input id="password" className="login__password" type="password" required />
           <span className="placeholder">Password</span>
         </label>
       </div>
